3c24be94018ffa0565c88138448156ebe3a7cadf
pointfeev <zehmeen@gmail.com>
2025-04-24 20:03:32 -0400

    Documentation and output updates

--- src/main/java/MealPlanner/DatabaseHelper.java
+++ src/main/java/MealPlanner/DatabaseHelper.java
@@ -10,8 +10,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Properties;
 
+import static MealPlanner.Main.outputException;
+
 public class DatabaseHelper {
-    final static int LOGIN_TIMEOUT = 3;
+    private final static int LOGIN_TIMEOUT = 3;
 
     private static Connection connection = null;
 
@@ -29,13 +31,17 @@ public class DatabaseHelper {
 
         try {
             DriverManager.registerDriver(new OracleDriver()); // optional, but just in case
+
             DriverManager.setLoginTimeout(LOGIN_TIMEOUT);
             connection = DriverManager.getConnection(url, username, password);
+
+            return true;
+        } catch (SQLTimeoutException exception) {
+            outputException("Connection timed out after %d seconds!".formatted(LOGIN_TIMEOUT));
         } catch (SQLException exception) {
-            handleSqlException(exception);
-            return false;
+            outputException(exception);
         }
-        return true;
+        return false;
     }
 
     /**
@@ -45,7 +51,7 @@ public class DatabaseHelper {
         try {
             connection.close();
         } catch (SQLException exception) {
-            handleSqlException(exception);
+            outputException(exception);
         }
         connection = null;
     }
@@ -56,7 +62,7 @@ public class DatabaseHelper {
      * @return The prepared statement with parameters (if any) bound to it; see {@link Connection#prepareStatement(String)} and {@link OraclePreparedStatement#setObject(int, Object)}
      * @throws SQLException May be thrown be either {@link Connection#prepareStatement(String)} or {@link OraclePreparedStatement#setObject(int, Object)}
      */
-    private static OraclePreparedStatement prepareStatement(String sql, Object... parameters) throws SQLException {
+    public static OraclePreparedStatement prepareStatement(String sql, Object... parameters) throws SQLException {
         OraclePreparedStatement statement = (OraclePreparedStatement) connection.prepareStatement(sql);
         for (int index = 0; index < parameters.length; index++) {
             statement.setObject(index, parameters[index]);
@@ -65,9 +71,10 @@ public class DatabaseHelper {
     }
 
     /**
-     * @param sql        SQL statement to prepare and execute; see {@link Connection#prepareStatement(String)}
-     * @param parameters Parameters to bind to the SQL statement (if any); see {@link OraclePreparedStatement#setObject(int, Object)}
-     * @return An array of hash maps, with each hash map corresponding to a row in the result set (see {@link OraclePreparedStatement#executeQuery()}),
+     * @param sql        SQL statement to prepare and execute; see {@link #prepareStatement(String, Object...)}
+     * @param parameters Parameters to bind to the SQL statement (if any); see {@link #prepareStatement(String, Object...)}
+     * @return If the query was not successful, {@code null}; otherwise, an array of hash maps,
+     * with each hash map corresponding to a row in the result set (see {@link OraclePreparedStatement#executeQuery()}),
      * each hash map key corresponding to a column name/label in that row (see {@link ResultSetMetaData#getColumnLabel(int)}),
      * and each hash map value corresponding to that column's value in that row (see {@link ResultSet#getObject(int)})
      */
@@ -90,49 +97,43 @@ public class DatabaseHelper {
             }
             return results;
         } catch (SQLException exception) {
-            handleSqlException(exception);
+            outputException(exception);
         }
         return null;
     }
 
     /**
-     * @param sql        SQL statement to prepare and execute; see {@link Connection#prepareStatement(String)}
-     * @param parameters Parameters to bind to the SQL statement (if any); see {@link OraclePreparedStatement#setObject(int, Object)}
-     * @return Number of rows updated by the query; see {@link OraclePreparedStatement#executeUpdate()}
+     * @param sql        SQL statement to prepare and execute; see {@link #prepareStatement(String, Object...)}
+     * @param parameters Parameters to bind to the SQL statement (if any); see {@link #prepareStatement(String, Object...)}
+     * @return If the query was not successful, {@code -1}; otherwise, the number of rows updated by the query (see {@link OraclePreparedStatement#executeUpdate()})
      */
     public static int executeUpdate(String sql, Object... parameters) {
         try (OraclePreparedStatement statement = prepareStatement(sql, parameters)) {
             return statement.executeUpdate();
         } catch (SQLException exception) {
-            handleSqlException(exception);
+            outputException(exception);
         }
         return -1;
     }
 
-    public static void handleSqlException(SQLException exception) {
-        if (exception instanceof SQLTimeoutException) {
-            // TODO
-            System.out.printf("Connection timed out after %d seconds!\n", LOGIN_TIMEOUT);
-            return;
-        }
-
-        // TODO
-        exception.printStackTrace();
-        // JOptionPane.showMessageDialog(null, e);
-    }
-
+    /**
+     * @return If an error occurred while reading the {@code database.properties} resource file, {@code null};
+     * otherwise, a {@link Properties} object containing the properties in the {@code database.properties} resource file
+     * (see {@link ClassLoader#getResourceAsStream(String)} and {@link Properties#load(InputStream)})
+     */
     private static Properties getProperties() {
-        Properties properties = new Properties();
         try (InputStream stream = DatabaseHelper.class.getClassLoader().getResourceAsStream("database.properties")) {
             if (stream == null) {
-                // TODO
-                System.out.println("Failed to obtain database properties! Make sure to follow the instructions in the README.");
+                outputException("Failed to obtain database properties! Make sure to follow the instructions in the README.");
                 return null;
             }
+
+            Properties properties = new Properties();
             properties.load(stream);
-        } catch (IOException e) {
-            // TODO
+            return properties;
+        } catch (IOException exception) {
+            outputException(exception);
         }
-        return properties;
+        return null;
     }
 }
--- src/main/java/MealPlanner/Main.java
+++ src/main/java/MealPlanner/Main.java
@@ -1,5 +1,7 @@
 package MealPlanner;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,5 +29,28 @@ public class Main {
             DatabaseHelper.disconnect();
             System.out.println("Disconnected from the database.");
         }
+    }
+
+    /**
+     * Converts the output of {@link Exception#printStackTrace()} to a {@link String} and sends it to {@link #outputException(String)}
+     *
+     * @param exception Exception to output the stack trace for
+     */
+    public static void outputException(Exception exception) {
+        StringWriter errors = new StringWriter();
+        exception.printStackTrace(new PrintWriter(errors));
+        outputException(errors.toString());
+    }
+
+    /**
+     * TODO: For now, sends the {@code message} to {@code System.out.println}; later, this will display a dialog with the {@code message}
+     * <p>
+     * TODO: This method and {@link #outputException(Exception)} will probably be moved to the class that handles the UI once that is developed
+     *
+     * @param message Message to output
+     */
+    public static void outputException(String message) {
+        System.out.println(message);
+        // JOptionPane.showMessageDialog(null, e);
     }
 }
