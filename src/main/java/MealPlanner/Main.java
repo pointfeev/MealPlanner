c9812512a8b538c39fb65e4c0fb15e4fd894e99d
pointfeev <zehmeen@gmail.com>
2025-05-05 01:24:00 -0400

    Clean up main tests

--- src/main/java/MealPlanner/Main.java
+++ src/main/java/MealPlanner/Main.java
@@ -3,11 +3,6 @@ package MealPlanner;
 import MealPlanner.GUI.MainFrame;
 import MealPlanner.Models.FoodItem;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 import static MealPlanner.GUI.MainFrame.displayInfoDialog;
 
 public class Main {
@@ -17,47 +12,21 @@ public class Main {
 
         DatabaseHelper.connect();
         DatabaseHelper.setup();
+
         FoodItem foodItemTest = new FoodItem();
         foodItemTest.name = "White Sandwich Bread";
         foodItemTest.unit = "slice";
         foodItemTest.insert();
         displayInfoDialog("inserted %s", foodItemTest.id);
+
         foodItemTest.food_group = "grains";
         foodItemTest.update();
         displayInfoDialog("updated %s", foodItemTest.id);
+
         foodItemTest.delete();
         displayInfoDialog("deleted %s", foodItemTest.id);
-        System.exit(0);
 
-        /*StringBuilder testString = new StringBuilder();
-        try {
-            testString.append("Connecting to the database...\n");
-            if (!DatabaseHelper.connect()) {
-                return;
-            }
-            testString.append("Setting up the database...\n");
-            if (!DatabaseHelper.setup()) {
-                return;
-            }
-            testString.append("Connected successfully, running a query...\n");
-            ArrayList<HashMap<String, Object>> results;
-            try {
-                results = DatabaseHelper.executeQuery("SELECT table_name FROM all_tables WHERE owner = SYS_CONTEXT('USERENV', 'CURRENT_USER')");
-            } catch (SQLException exception) {
-                testString.append("Query failed!\n");
-                return;
-            }
-            for (HashMap<String, Object> result : results) {
-                for (Map.Entry<String, Object> entry : result.entrySet()) {
-                    testString.append("%s: %s\n".formatted(entry.getKey(), entry.getValue()));
-                }
-            }
-            testString.append("Query successful!\n");
-        } finally {
-            DatabaseHelper.disconnect();
-            testString.append("Disconnected from the database.\n");
-            displayInfoDialog(testString.toString());
-            System.exit(0);
-        }*/
+        DatabaseHelper.disconnect();
+        System.exit(0);
     }
 }
