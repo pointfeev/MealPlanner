e74926c1fd53df4e022e5db8f8a13cbeda141bf3
pointfeev <zehmeen@gmail.com>
2025-05-05 20:39:28 -0400

    Add crude display for recipes

--- src/main/java/MealPlanner/Forms/MainFrame.java
+++ src/main/java/MealPlanner/Forms/MainFrame.java
@@ -2,12 +2,13 @@ package MealPlanner.Forms;
 
 import MealPlanner.DatabaseHelper;
 import MealPlanner.Forms.Components.PlaceholderTextField;
-import MealPlanner.Models.Annotations.Ignore;
+import MealPlanner.Models.FoodItem;
 import MealPlanner.Models.Recipe;
+import MealPlanner.Models.RecipeIngredient;
+import MealPlanner.Models.RecipeInstruction;
 
 import javax.swing.*;
 import java.awt.*;
-import java.lang.reflect.Field;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static MealPlanner.Main.*;
@@ -16,6 +17,8 @@ public class MainFrame extends JFrame {
     public JPanel contentPane;
     public PlaceholderTextField recipeSearchField;
     public JTabbedPane tabbedPane;
+    public JScrollPane recipesScrollPane;
+    public JPanel recipesPanel;
 
     public MainFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -27,6 +30,9 @@ public class MainFrame extends JFrame {
         $$$setupUI$$$();
 
         setupRecipeTab();
+        setupMealPlansTab();
+        setupFridgeTab();
+        setupShoppingListTab();
 
         setContentPane(contentPane);
         setVisible(true);
@@ -34,12 +40,20 @@ public class MainFrame extends JFrame {
         initialize();
 
         populateRecipeTab(); // initial tab
-        tabbedPane.addChangeListener(event -> {
+        tabbedPane.addChangeListener(e -> {
             switch (tabbedPane.getSelectedIndex()) {
                 case 0:
                     populateRecipeTab();
                     break;
-                // TODO
+                case 1:
+                    populateMealPlansTab();
+                    break;
+                case 2:
+                    populateFridgeTab();
+                    break;
+                case 3:
+                    populateShoppingListTab();
+                    break;
                 default:
                     tabbedPane.setSelectedIndex(0);
                     break;
@@ -73,26 +87,89 @@ public class MainFrame extends JFrame {
     }
 
     public void setupRecipeTab() {
+        recipesPanel = new JPanel();
+        recipesPanel.setLayout(new BoxLayout(recipesPanel, BoxLayout.Y_AXIS));
+        recipesPanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+        recipesScrollPane.setViewportView(recipesPanel);
+
         recipeSearchField.addActionListener(event -> {
             // TODO
         });
     }
 
     public void populateRecipeTab() {
+        recipesPanel.removeAll();
+
         Recipe[] recipes = new Recipe().select();
         if (recipes == null) {
             return;
         }
 
         for (Recipe recipe : recipes) {
-            for (Field field : Recipe.class.getFields()) {
-                if (field.getAnnotation(Ignore.class) != null) {
+            RecipePanel recipePanel = new RecipePanel();
+            recipePanel.categoryLabel.setText(recipe.category);
+            recipePanel.nameLabel.setText(recipe.name);
+            recipePanel.editButton.addActionListener(event -> {
+                // TODO
+            });
+
+            RecipeIngredient recipeIngredientCriteria = new RecipeIngredient();
+            recipeIngredientCriteria.recipe_id = recipe.id;
+            RecipeIngredient[] recipeIngredients = recipeIngredientCriteria.select();
+            for (RecipeIngredient recipeIngredient : recipeIngredients) {
+                FoodItem foodItemCriteria = new FoodItem();
+                foodItemCriteria.id = recipeIngredient.food_id;
+                FoodItem[] foodItems = foodItemCriteria.select();
+                if (foodItems == null || foodItems.length == 0) {
                     continue;
                 }
+                FoodItem foodItem = foodItems[0];
 
-                // TODO
+                RecipeIngredientPanel recipeIngredientPanel = new RecipeIngredientPanel();
+                recipeIngredientPanel.label.setText("%s %s(s) of %s".formatted(recipeIngredient.quantity, foodItem.unit, foodItem.name));
+
+                recipeIngredientPanel.updateSize();
+                recipePanel.contentPane.add(recipeIngredientPanel.contentPane);
             }
+
+            RecipeInstruction recipeInstructionCriteria = new RecipeInstruction();
+            recipeInstructionCriteria.recipe_id = recipe.id;
+            RecipeInstruction[] recipeInstructions = recipeInstructionCriteria.select();
+            for (RecipeInstruction recipeInstruction : recipeInstructions) {
+                RecipeInstructionPanel recipeInstructionPanel = new RecipeInstructionPanel();
+                recipeInstructionPanel.label.setText("%s. %s".formatted(recipeInstruction.step, recipeInstruction.instruction));
+
+                recipeInstructionPanel.updateSize();
+                recipePanel.contentPane.add(recipeInstructionPanel.contentPane);
+            }
+
+            recipePanel.updateSize();
+            recipesPanel.add(recipePanel.contentPane);
         }
+    }
+
+    public void setupMealPlansTab() {
+        // TODO
+    }
+
+    public void populateMealPlansTab() {
+        // TODO
+    }
+
+    public void setupFridgeTab() {
+        // TODO
+    }
+
+    public void populateFridgeTab() {
+        // TODO
+    }
+
+    public void setupShoppingListTab() {
+        // TODO
+    }
+
+    public void populateShoppingListTab() {
+        // TODO
     }
 
     /**
@@ -138,8 +215,9 @@ public class MainFrame extends JFrame {
         panel2.setLayout(new BorderLayout(0, 0));
         panel1.add(panel2, BorderLayout.NORTH);
         panel2.add(recipeSearchField, BorderLayout.CENTER);
-        final JScrollPane scrollPane1 = new JScrollPane();
-        panel1.add(scrollPane1, BorderLayout.CENTER);
+        recipesScrollPane = new JScrollPane();
+        recipesScrollPane.setHorizontalScrollBarPolicy(30);
+        panel1.add(recipesScrollPane, BorderLayout.CENTER);
         final JPanel panel3 = new JPanel();
         panel3.setLayout(new BorderLayout(0, 0));
         tabbedPane.addTab("Meal Plans", panel3);
